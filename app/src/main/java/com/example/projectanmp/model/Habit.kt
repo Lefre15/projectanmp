7615a17f83732114ce32fafe35cba9e7a25bb0b8
Lefre15 <leonardsutanto54@gmail.com>
2026-04-20 19:49:56 +0700

    init

---
+++ app/src/main/java/com/example/projectanmp/model/Habit.kt
@@ -0,0 +1,11 @@
+package com.example.projectanmp.model
+
+data class Habit(
+    var id: String?,
+    var name: String?,
+    var description: String?,
+    var goal: Int?,
+    var unit: String?,
+    var icon: String?,
+    var progress: Int?
+)
